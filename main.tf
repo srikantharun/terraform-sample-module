962cbe07d82ada90644b1da5da70abbd4aa6b52d
srikantharun <srikanth.arunachalam@googlemail.com>
2023-02-26 20:53:50 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,31 @@
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+resource "github_repository" "new-repository" {
+  name               = "${var.repo["name"]}"
+  description        = "${var.repo["description"]}"
+  private            = "${lookup(var.repo, "private", false)}"
+  auto_init          = "${lookup(var.repo, "auto_init", true)}"
+  gitignore_template = "${var.repo["gitignore_template"]}"
+  license_template   = "${var.repo["license"]}"
+  homepage_url       = "${lookup(var.repo, "homepage_url", "")}"
 
+  has_issues         = "${lookup(var.repo, "enable_issues", true)}"
+  has_wiki           = "${lookup(var.repo, "enable_wiki", true)}"
+  has_downloads      = "${lookup(var.repo, "enable_download", true)}"
+  allow_merge_commit = "${lookup(var.repo, "enable_merge_commit", true)}"
+  allow_squash_merge = "${lookup(var.repo, "enable_squash_merge", true)}"
+  allow_rebase_merge = "${lookup(var.repo, "enable_rebase_merge", true)}"
+}
 
-module "vpc" {
-  source  = "terraform-google-modules/network/google"
-  version = "3.3.0"
+resource "github_repository_webhook" "new-repository-webhook" {
+  count      = "${length(var.webhook)}"
+  repository = "${github_repository.new-repository.name}"
+  name       = "${lookup(var.webhook[count.index], "name")}"
 
-  project_id   = "${var.project}"
-  network_name = "${var.env}"
-
-  subnets = [
-    {
-      subnet_name   = "${var.env}-subnet-01"
-      subnet_ip     = "10.${var.env == "dev" ? 10 : 20}.10.0/24"
-      subnet_region = "us-west1"
-    },
-  ]
-
-  secondary_ranges = {
-    "${var.env}-subnet-01" = []
+  configuration {
+    url          = "${lookup(var.webhook[count.index], "url")}"
+    content_type = "${lookup(var.webhook[count.index], "content_type")}"
+    insecure_ssl = "${lookup(var.webhook[count.index], "insecure_ssl")}"
   }
+
+  active = "${lookup(var.webhook[count.index], "enable", true)}"
+  events = "${split(",", lookup(var.webhook[count.index], "events"))}"
 }
